a8968943cb559c7c4e17335351c60398912b61b4
Keith Dimatteo <kdimat@boskdima24048.wellmanage.com>
2013-03-21 08:25:49 -0400

    rev

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 AustraliaJS
 -----------
 
-With respect to @awsan (really, it was his idea).
+With respect to https://github.com/alasdairswan (really, it was his idea).
 
 I still need to verify this on a phone but you get the idea.
---
+++ australia.js
@@ -0,0 +1,60 @@
+(function( $ ) {
+    "use strict";
+
+    $.fn.australia = function() {
+
+        function getsupportedprop(proparray){
+            var root = document.documentElement; //reference root element of document
+            for (var i=0; i<proparray.length; i++){ //loop through possible properties
+                if (typeof root.style[proparray[i]] === "string"){ //if the property value is a string (versus undefined)
+                    return proparray[i]; //return that string
+                }
+            }
+        }
+
+        function changecssproperty(target, prop, value, action){
+            if ( typeof prop !== undefined ) {
+                if(action === "remove"){
+                    target.style[prop] = "";
+                } else {
+                    target.style[prop] = value;
+                }
+            }
+        }
+
+        var csstransform = getsupportedprop(["transform", "MozTransform", "WebkitTransform", "msTransform", "OTransform"]);
+        changecssproperty(document.body, csstransform, "rotate(180deg)");
+
+
+        $(window).bind("orientationchange", function(e) {
+
+            switch ( window.orientation ) {
+
+            case 0:
+                //alert("portrait mode");
+                changecssproperty(document.body, csstransform, "rotate(180deg)");
+                break;
+
+            case 90:
+                changecssproperty(document.body, csstransform, "rotate(-90deg)");
+                //alert("landscape mode screen turned to the left");
+                break;
+
+            case -90:
+                changecssproperty(document.body, csstransform, "rotate(-90deg)");
+                //alert("landscape mode screen turned to the right");
+                break;
+
+            }
+        });
+
+    };
+
+
+    $(document).ready(function(){
+        $(this).australia();
+    });
+
+
+})(jQuery);
+
